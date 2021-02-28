58b512e34d8d80b250b2e3e14fd902934359b018
Skurdt <skurdt@gmail.com>
2021-02-28 22:22:56 +0100

    1 liner...

--- Runtime/Math/MathUtils.cs
+++ Runtime/Math/MathUtils.cs
@@ -27,13 +27,6 @@ namespace SK.Utilities
     public static class MathUtils
     {
         public static T Clamp<T>(this T val, T min, T max) where T : IComparable<T>
-        {
-            if (val.CompareTo(min) < 0)
-                return min;
-            else if (val.CompareTo(max) > 0)
-                return max;
-            else
-                return val;
-        }
+            => val.CompareTo(min) < 0 ? min : val.CompareTo(max) > 0 ? max : val;
     }
 }
